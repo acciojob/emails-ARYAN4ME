b6a39fd69c5b6c9e29bea7b6efef266745117e0e
Mohit Prajapati <skparjapati9045@gmail.com>
2023-02-18 15:29:32 +0530

    Second commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -20,27 +20,6 @@ public class Email {
         return password;
     }
 
-
-    public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(password.equals(oldPassword)){
-            if(isCheck(newPassword)){
-                System.out.println("Congratulation password changed successfully");
-                this.password = newPassword;
-            }
-            else{
-                System.out.println("This is Invalid");
-            }
-        }
-        else{
-            System.out.println("The given password is not same:");
-        }
-    }
     private boolean isCheck(String password){
         boolean capitalLetter = false;
         boolean smallLetter = false;
@@ -67,5 +46,23 @@ public class Email {
             }
         }
         return capitalLetter && smallLetter && digit && specialCharacter ;
+    }
+    public void changePassword(String oldPassword, String newPassword) {
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (password.equals(oldPassword)) {
+            if (isCheck(newPassword)) {
+                System.out.println("Congratulation password changed successfully");
+                this.password = newPassword;
+            } else {
+                System.out.println("This is Invalid");
+            }
+        } else {
+            System.out.println("The given password is not same:");
+        }
     }
 }
